17d2cb3a3fc9e8950ab0cf6f8ad02b6165d0151a
rkdrjsrks <rkdrjs71@naver.com>
2024-02-03 20:50:11 +0900

    프로그래머스 연습

---
+++ src/main/java/org/example/test/practice/Test.java
@@ -0,0 +1,21 @@
+package org.example.test.practice;
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        int num_list[] = {3, 4, 5, 2, 1};
+        int result1 = 0;
+        int result2 = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 != 0) {
+                result1 *= 10;
+                result1 += num_list[i];
+            } else {
+                result2 *= 10;
+                result2 += num_list[i];
+            }
+        }
+        System.out.println(result1 + result2);
+    }
+}
