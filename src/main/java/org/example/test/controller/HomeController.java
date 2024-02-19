890208f47426c907ad7f4015e58fc376b0f58c11
rkdrjsrks <rkdrjs71@naver.com>
2024-02-20 01:39:48 +0900

    타임리프 연습

--- src/main/java/org/example/test/controller/HomeController.java
+++ src/main/java/org/example/test/controller/HomeController.java
@@ -1,154 +1,40 @@
 package org.example.test.controller;
 
-import org.example.test.domain.Category;
-import org.example.test.domain.TestDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
     @GetMapping
-    public String homecontroller(Model model){
-        List<TestDto> categories = List.of(
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리1",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                ),
-                new TestDto(
-                        "대 카테고리2",
-                        List.of(
-                                new Category("중 카테고리1"
-                                ),
-                                new Category("중 카테고리2"
-                                ),
-                                new Category("중 카테고리3"
-                                )
-                        )
-                )
-        );
-        model.addAttribute("categories",categories);
+    public String home(Model model){
         model.addAttribute("content", "main-content");
-        return "layout";
+        return "shop";
     }
     @GetMapping("/myPage")
     public String myPage(Model model) {
         model.addAttribute("content", "myPage");
-
-        return "layout";
+        return "shop";
     }
     @GetMapping("/notice")
     public String noticePage(Model model) {
         model.addAttribute("content", "notice");
 
-        return "layout";
+        return "shop";
     }
     @GetMapping("/allPage")
     public String allPage(Model model) {
         model.addAttribute("content", "allPage");
-        return "layout";
+        return "shop";
     }
     @GetMapping("/login")
-    public String logincontroller(){
+    public String login(){
         return "login";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "register";
     }
 }
