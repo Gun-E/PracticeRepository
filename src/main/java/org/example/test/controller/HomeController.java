a230b4cdcdf047f8f526f651a1774f23e667089a
rkdrjsrks <rkdrjs71@naver.com>
2024-02-28 05:10:40 +0900

    타임리프 연습

--- src/main/java/org/example/test/controller/HomeController.java
+++ src/main/java/org/example/test/controller/HomeController.java
@@ -10,35 +10,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping
     public String home(Model model){
-        model.addAttribute("content", "main-content");
-        return "shop";
+        model.addAttribute("content", "product-list-starred");
+        return "layout";
     }
     @GetMapping("/myPage")
     public String myPage(Model model) {
-        model.addAttribute("content", "myPage");
-        return "shop";
+        model.addAttribute("content", "user-detail");
+        return "layout";
     }
     @GetMapping("/notice")
     public String noticePage(Model model) {
         model.addAttribute("content", "notice");
 
-        return "shop";
+        return "layout";
     }
     @GetMapping("/allPage")
     public String allPage(Model model) {
-        model.addAttribute("content", "allPage");
-        return "shop";
+        model.addAttribute("content", "product-list");
+        return "layout";
     }
     @GetMapping("/modify")
     public String modify(Model model) {
-        model.addAttribute("content", "modify");
-        return "shop";
+        model.addAttribute("content", "user-detail-modify");
+        return "layout";
     }
     @GetMapping("/detail/1")
     public String detail(Model model){
-        model.addAttribute("content", "detail");
+        model.addAttribute("content", "product-detail");
         model.addAttribute("won", 6000);
-        return "shop";
+        return "layout";
+    }
+    @GetMapping("/cart")
+    public String cart(Model model){
+        model.addAttribute("content", "cart");
+        return "layout";
     }
     @GetMapping("/login")
     public String login(){
