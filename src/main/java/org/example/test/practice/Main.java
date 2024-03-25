19504fccdff8a0394dcdf7937979ff312f9f8ae0
rkdrjsrks <rkdrjs71@naver.com>
2024-03-25 20:25:09 +0900

    백준 알고리즘 연습

--- src/main/java/org/example/test/practice/Main.java
+++ src/main/java/org/example/test/practice/Main.java
@@ -1,13 +1,9 @@
 package org.example.test.practice;
 
-import java.util.Scanner;
+import java.time.LocalDate;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int w =sc.nextInt();
-        int h =sc.nextInt();
-
-        System.out.println(w * h -1);
+        System.out.println(LocalDate.now());
     }
 }
