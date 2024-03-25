64587cc1c954082c2f517bf9172ec94966386b58
rkdrjsrks <rkdrjs71@naver.com>
2024-03-25 20:55:12 +0900

    백준 알고리즘 연습

--- src/main/java/org/example/test/practice/Main.java
+++ src/main/java/org/example/test/practice/Main.java
@@ -1,9 +1,22 @@
 package org.example.test.practice;
 
-import java.time.LocalDate;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println(LocalDate.now());
+        Scanner sc = new Scanner(System.in);
+        int h  = sc.nextInt();
+        int m = sc.nextInt();
+        int s = sc.nextInt();
+
+        int input = sc.nextInt();
+
+        int  a = (h* (3600)) + (m * 60) + s;
+
+        a+=input;
+
+        System.out.print(a/(3600)%24 + " ");
+        System.out.print(a/60%60 + " ");
+        System.out.print(a%60);
     }
 }
