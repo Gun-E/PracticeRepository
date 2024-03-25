141e54cfb900e3598a85b8c02e7e429ce9af1b80
rkdrjsrks <rkdrjs71@naver.com>
2024-03-25 21:50:04 +0900

    백준 알고리즘 연습

--- src/main/java/org/example/test/practice/Main.java
+++ src/main/java/org/example/test/practice/Main.java
@@ -4,19 +4,7 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h  = sc.nextInt();
-        int m = sc.nextInt();
-        int s = sc.nextInt();
-
-        int input = sc.nextInt();
-
-        int  a = (h* (3600)) + (m * 60) + s;
-
-        a+=input;
-
-        System.out.print(a/(3600)%24 + " ");
-        System.out.print(a/60%60 + " ");
-        System.out.print(a%60);
+        System.out.println(78);
+        System.out.println("rkdrjs71");
     }
 }
